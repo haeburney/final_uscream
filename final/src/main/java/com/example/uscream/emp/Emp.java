1a372bdfbee5575d538674b71b3b67a2823d6ac0
haeburney <4ckdnjssu@naver.com>
2023-06-20 15:23:58 +0900

    emp, worklogs, Basicschedule 테이블 생성
    2023-06-29 15:23pm

--- final/src/main/java/com/example/uscream/emp/Emp.java
+++ final/src/main/java/com/example/uscream/emp/Emp.java
@@ -1,5 +1,46 @@
 package com.example.uscream.emp;
 
-public class Emp {
+import java.util.Date;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.example.uscream.store.Store;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Emp {
+	@Id
+	@SequenceGenerator(name="seq_emp", sequenceName="seq_emp", allocationSize =1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_emp")
+	private int empnum; 	// 직원 번호
+	
+	@ManyToOne
+	@JoinColumn(name="storeid", nullable=false)
+	@OnDelete(action=OnDeleteAction.CASCADE)
+	private Store storeid;	// 지점 번호
+	
+	private String empname;	// 직원 이름
+	private Date joindate;	// 입사 날짜
+	private int salary;		// 급여
+	private int grade;		// 등급
+	private String color;	// 컬러 
+	
 }
