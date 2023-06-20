b279904c14daca002a6b7be2e9f5b06bc0b078af
KOSTA <KOSTA@DESKTOP-3U4FAHI>
2023-06-20 17:39:04 +0900

    msg todo
    0620 17:00
    
    컨트롤러 시작

--- final/src/main/java/com/example/uscream/msg/MsgDao.java
+++ final/src/main/java/com/example/uscream/msg/MsgDao.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.uscream.account.Account;
+import com.example.uscream.store.Store;
 	
 @Repository
 public interface MsgDao extends JpaRepository<Msg, Integer> {
@@ -19,52 +20,52 @@ public interface MsgDao extends JpaRepository<Msg, Integer> {
 	ArrayList<Msg> findByReceiver(String name);		//내가 받은 메일 검색 
 	ArrayList<Msg> findByTitleLike(String title);
 	ArrayList<Msg> findAll(String title);
-	ArrayList<Msg> findByReceiverAndSender(Account account);	
+	ArrayList<Msg> findByReceiverAndSender(Store store);	
 	
 	@Modifying
 	@Transactional
-	@Query(value="delete * from msg where and receiver = ? ", nativeQuery=true)
-	ArrayList<Msg> deleteByReceiver(Account receiver);
+	@Query(value="delete * from msg where and receiver = :Store ", nativeQuery=true)
+	ArrayList<Msg> deleteByReceiver(@Param("Store") Store receiver);
 	
 	
 	//전체 즐찾 조회 
-	@Query(value="select count(*)  from msg where mark = 1 ", nativeQuery=true)
-	long countByMark();
+	@Query(value="select count(*)  from msg where mark = 1 and receiver=:Store", nativeQuery=true)
+	long countByMark(@Param("Store") Store store);
 	// 전체 즐찾 중 읽지 않은 메시지 
-	@Query(value="select count(*)  from msg where mark = 1 and readcheck = 1 ", nativeQuery=true)
-	long countByMarkRead();
+	@Query(value="select count(*)  from msg where mark = 1 and readcheck = 1 and receiver=:Store", nativeQuery=true)
+	long countByMarkRead(@Param("Store") Store store);
 	
 	
 	// 수신자로 메일 수 조회 
-	@Query(value="select count(*)  from msg where receiver = :", nativeQuery=true)
-	long countAll(@Param("") );
+	@Query(value="select count(*)  from msg where receiver = :Store", nativeQuery=true)
+	long countAll(@Param("Store") Store store);
 	// 수신자 메일 중 읽지 않은 메시지 
-	@Query(value="select count(*)  from msg where receiver = : and readcheck = 0 ", nativeQuery=true)
-	long countAllRead(@Param(""));
+	@Query(value="select count(*)  from msg where receiver = :Store and readcheck = 0 ", nativeQuery=true)
+	long countAllRead(@Param("Store") Store store);
 	
 	
 	//보낸 메일 메시지 
-	@Query(value="select count(*)  from msg where vender = :account.vender ", nativeQuery=true)
-	long countByVender(@Param("account") Account account);
+	@Query(value="select count(*)  from msg where sender = :Store ", nativeQuery=true)
+	long countByVender(@Param("Store") Store store);
 	//보낸 메일 중 읽지 않은 메시지 
-	@Query(value="select count(*)  from msg where tempcheck =1 vender =:account.vender ", nativeQuery=true)
-	long countByVenderTemp(@Param("account") Account account);
+	@Query(value="select count(*)  from msg where tempcheck =1 sender =:Store ", nativeQuery=true)
+	long countByVenderTemp(@Param("Store") Store store);
 	
 	
 	// 임시보관 전체
-	@Query(value="select count(*)  from msg where tempcheck = 1 and sender = :account.sender ", nativeQuery=true)
-	long countByTemp(@Param("account") Account account);
+	@Query(value="select count(*)  from msg where tempcheck = 1 and sender = :Store", nativeQuery=true)
+	long countByTemp(@Param("Store") Store store);
 	// 임시보관에서 읽지 않은 메세지
-	@Query(value="select count(*)  from msg where tempcheck = 1 and readcheck = 0 and sender = :account.sender ", nativeQuery=true)
-	long countByTempRead(@Param("account") Account account);
-
+	@Query(value="select count(*)  from msg where tempcheck = 1 and readcheck = 0 and sender = :Store ", nativeQuery=true)
+	long countByTempRead(@Param("Store") Store store);
+		
 		
 	// 휴지통 전체
-	@Query(value="select count(*)  from msg where delcheck = 1 and receiver = :account", nativeQuery=true)
-	long countByDelcheck(@Param("account") Account receiver);
+	@Query(value="select count(*)  from msg where delcheck = 1 and receiver = :Store", nativeQuery=true)
+	long countByDelcheck(@Param("Store") Store receiver);
 	// 휴지통에서 읽지 않은 메시지  
-	@Query(value="select count(*) from msg where delcheck = 1 and readcheck = 0 and receiver = :account", nativeQuery=true)
-	long countByDelRead(@Param("account") Account account);
+	@Query(value="select count(*) from msg where delcheck = 1 and readcheck = 0 and receiver = :Store", nativeQuery=true)
+	long countByDelRead(@Param("Store") Store receiver);
 	
 	//account.sender ,, account.receiver = > loginId ; 
 	
