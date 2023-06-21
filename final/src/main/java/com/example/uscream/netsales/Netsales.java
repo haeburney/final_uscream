28b45b5eabb5fa73680db0335e1976b2cf15c6f4
YK <YK@DESKTOP-G9CIKTG>
2023-06-21 12:07:36 +0900

    Netsales
    2023-06-21 12:06
    서버 실행 오류 해결을 위한 수정 (조인 컬럼 @ManyToOne 추가)

--- final/src/main/java/com/example/uscream/netsales/Netsales.java
+++ final/src/main/java/com/example/uscream/netsales/Netsales.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -29,15 +30,19 @@ public class Netsales {
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seq_netsales")
 	private int netsalesnum;
 	
+	@ManyToOne
 	@JoinColumn(name="storeid", nullable=false)
 	private Store store;
 	
+	@ManyToOne
 	@JoinColumn(name="selling", nullable=false)
 	private Selling selling;
 	
+	@ManyToOne
 	@JoinColumn(name="monthlypay", nullable=false)
 	private Monthlypay monthlypay;
 	
+	@ManyToOne
 	@JoinColumn(name="order", nullable=false)
 	private Order order;
 
