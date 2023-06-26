59c1b7e1dd4148e4516b29b4ccd8a1e18e86f4a9
haeburney <4ckdnjssu@naver.com>
2023-06-26 17:41:16 +0900

    Monthlypay 자동 함수 생성 & Schedule & Store 수정

--- final/src/main/java/com/example/uscream/schedule/ScheduleService.java
+++ final/src/main/java/com/example/uscream/schedule/ScheduleService.java
@@ -13,56 +13,67 @@ import com.example.uscream.store.Store;
 public class ScheduleService {
 	@Autowired
 	ScheduleDao dao;
-	
-	// 추가, 수정 
+
+	// 추가, 수정
 	public ScheduleDto save(ScheduleDto dto) {
-		Schedule entity = dao.save(new Schedule(dto.getSnum(), dto.getEmp(), dto.getStoreid(), dto.getSdate(), dto.getStarttime(), dto.getEndtime()));
-		return new ScheduleDto(entity.getSnum(), entity.getEmp(), entity.getStoreid(), entity.getSdate(), entity.getStarttime(), entity.getEndtime());
+		Schedule entity = dao.save(new Schedule(dto.getSnum(), dto.getEmp(), dto.getStoreid(), dto.getSdate(),
+				dto.getStarttime(), dto.getEndtime()));
+		return new ScheduleDto(entity.getSnum(), entity.getEmp(), entity.getStoreid(), entity.getSdate(),
+				entity.getStarttime(), entity.getEndtime());
 	}
-	
-	// num으로 (pk로) 검색 
+
+	// num으로 (pk로) 검색
 	public ScheduleDto getBySnum(int snum) {
 		Schedule entity = dao.findById(snum).orElse(null);
-		return new ScheduleDto(entity.getSnum(), entity.getEmp(), entity.getStoreid(), entity.getSdate(), entity.getStarttime(), entity.getEndtime());
+		if (entity == null) {
+			return null;
+		} else {
+			return new ScheduleDto(entity.getSnum(), entity.getEmp(), entity.getStoreid(), entity.getSdate(),
+					entity.getStarttime(), entity.getEndtime());
+
+		}
 	}
-	
+
 	// 직원 번호로 검색
-	public ArrayList<ScheduleDto> getByEmp(int empnum){
-		Emp e = new Emp(empnum, null, "" ,null, null, "");
+	public ArrayList<ScheduleDto> getByEmp(int empnum) {
+		Emp e = new Emp(empnum, null, "", null, null, "");
 		ArrayList<Schedule> list = dao.findByEmp(e);
 		ArrayList<ScheduleDto> dtoList = new ArrayList<ScheduleDto>();
-		for(Schedule vo:list) {
-			dtoList.add(new ScheduleDto(vo.getSnum(), vo.getEmp(), vo.getStoreid(), vo.getSdate(), vo.getStarttime(), vo.getEndtime()));
+		for (Schedule vo : list) {
+			dtoList.add(new ScheduleDto(vo.getSnum(), vo.getEmp(), vo.getStoreid(), vo.getSdate(), vo.getStarttime(),
+					vo.getEndtime()));
 		}
-		
+
 		return dtoList;
 	}
-	
+
 	// 지점 아이디로 검색
-	public ArrayList<ScheduleDto> getByStoreid(String storeid){
+	public ArrayList<ScheduleDto> getByStoreid(String storeid) {
 		Store s = new Store(storeid, "", "", "", 0, "", 0, 0);
 		ArrayList<Schedule> list = dao.findByStoreid(s);
 		ArrayList<ScheduleDto> dtoList = new ArrayList<ScheduleDto>();
-		for(Schedule vo:list) {
-			dtoList.add(new ScheduleDto(vo.getSnum(), vo.getEmp(), vo.getStoreid(), vo.getSdate(), vo.getStarttime(), vo.getEndtime()));
+		for (Schedule vo : list) {
+			dtoList.add(new ScheduleDto(vo.getSnum(), vo.getEmp(), vo.getStoreid(), vo.getSdate(), vo.getStarttime(),
+					vo.getEndtime()));
 		}
-		
+
 		return dtoList;
 	}
-	
+
 	// 직원 번호 & 날짜로 검색
 	public ArrayList<ScheduleDto> getByEmpAndDate(int empnum, LocalDate sdate) {
-		Emp e = new Emp(empnum, null, "" ,null, null, "");
+		Emp e = new Emp(empnum, null, "", null, null, "");
 		ArrayList<Schedule> list = dao.findByEmpAndSdate(e, sdate);
 		ArrayList<ScheduleDto> dtoList = new ArrayList<ScheduleDto>();
-		for(Schedule vo:list) {
-			dtoList.add(new ScheduleDto(vo.getSnum(), vo.getEmp(), vo.getStoreid(), vo.getSdate(), vo.getStarttime(), vo.getEndtime()));
+		for (Schedule vo : list) {
+			dtoList.add(new ScheduleDto(vo.getSnum(), vo.getEmp(), vo.getStoreid(), vo.getSdate(), vo.getStarttime(),
+					vo.getEndtime()));
 		}
-	
+
 		return dtoList;
 	}
-	
-	// 삭제 
+
+	// 삭제
 	public void delete(int snum) {
 		dao.deleteById(snum);
 	}
