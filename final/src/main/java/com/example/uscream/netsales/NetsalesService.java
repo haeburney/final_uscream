4c98f1070c63050176ab7dbbddadc6e28682d60a
YK <wyk160@naver.com>
2023-06-26 17:43:27 +0900

    SellingDao, SellingService, SellingController, netsalesDao, netSalesService, NetsalesController
    2023-06-26 17:43

--- final/src/main/java/com/example/uscream/netsales/NetsalesService.java
+++ final/src/main/java/com/example/uscream/netsales/NetsalesService.java
@@ -1,24 +1,47 @@
 package com.example.uscream.netsales;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.example.uscream.monthlypay.Monthlypay;
-import com.example.uscream.porder.Porder;
-import com.example.uscream.selling.Selling;
-import com.example.uscream.store.Store;
 
 @Service
 public class NetsalesService {
 	@Autowired
 	private NetsalesDao dao;
 	
-	// 테이블에 값 update (매월 1일에 전월 데이터 가져옴)
+	// 테이블에 값 update (매월 1일에 전월 데이터 저장)
 	public NetsalesDto save(NetsalesDto dto) {
 		dao.save(new Netsales(dto.getNetsalesnum(), dto.getNetsalesdate(), dto.getStoreid(), 
 				dto.getMsellingprice(), dto.getMpsalary(),dto.getMordercost(), dto.getMnetsales()));
 		
 		return dto;
 	}
+
+	// 전체 검색
+	public ArrayList<NetsalesDto> getAll() {
+		ArrayList<Netsales> list = (ArrayList<Netsales>) dao.findAll();
+		ArrayList<NetsalesDto> list2 = new ArrayList<NetsalesDto>();
+		for(Netsales vo : list) {
+			list2.add(new NetsalesDto(vo.getNetsalesnum(), vo.getNetsalesdate(), vo.getStoreid(), 
+					vo.getMsellingprice(), vo.getMpsalary(), vo.getMordercost(), vo.getMnetsales()));
+			
+		}
+		return list2;
+	}
+	
+	// 연도별 순매출
+	public ArrayList<Map<String, Object[]>> getBySelectYear(String storeid, int year) {
+		ArrayList<Map<String, Object[]>> list = dao.findNetsalesYear(storeid, year);
+		return list;
+	}
+	
+	// 월별 순매출
+	public ArrayList<Map<String, Object[]>> getBySelectYearAndMonth(String storeid, int year, int month) {
+		ArrayList<Map<String, Object[]>> list = dao.findYearAndMonth(storeid, year, month);
+		return list;
+	}
+	
 
 } 
--- final/src/main/java/com/example/uscream/selling/SellingController.java
+++ final/src/main/java/com/example/uscream/selling/SellingController.java
@@ -23,7 +23,7 @@ public class SellingController {
 	private SellingService service;
 	
 	// 판매내역 추가
-	@PostMapping("/add")
+	@PostMapping("/add") //
 	public Map add(SellingDto dto) {
 		SellingDto list = service.save(dto);
 		Map map = new HashMap();
@@ -32,7 +32,7 @@ public class SellingController {
 	}
 	
 	// 전체 판매내역 검색
-	@GetMapping("")
+	@GetMapping("")	//
 	public Map getAll() {
 		ArrayList<SellingDto> list = service.getAll();
 		Map map = new HashMap();
@@ -41,99 +41,99 @@ public class SellingController {
 	}
 		
 	// 일별 전체 매출
-	@GetMapping("dailysales/{year}/{month}")
+	@GetMapping("dailysales/{year}/{month}") //
 	public Map getByDaySales(@PathVariable("year") int year, @PathVariable("month") int month) {
-		ArrayList<Object[]> list = service.getByDailySales(year, month);
+		ArrayList<Map<String, Object[]>> list = service.getByDailySales(year, month);
 		Map map = new HashMap();
 		map.put("list", list);
 		return map;
 	}
 	
 	// 일별 특정지점 매출
-	@GetMapping("dailysales/{storeid}/{year}/{month}")
+	@GetMapping("dailysales/{storeid}/{year}/{month}") //
 	public Map getByStoreDaySales(@PathVariable("storeid") String storeid, @PathVariable("year") int year, @PathVariable("month") int month) {
-		ArrayList<Object[]> list = service.getByStoreDailySales(storeid, year, month);
+		ArrayList<Map<String, Object[]>> list = service.getByStoreDailySales(storeid, year, month);
 		Map map = new HashMap();
 		map.put("list", list);
 		return map;
 	}
 	
 	// 1년 단위 전체 월별 매출
-	@GetMapping("monthlysales/{year}")
+	@GetMapping("monthlysales/{year}") // * year 나오게 추가
 	public Map getByMonthSales(@PathVariable("year") int year) {
-		ArrayList<Object[]> list = service.getByMonthlySales(year);
+		ArrayList<Map<String, Object[]>> list = service.getByMonthlySales(year);
 		Map map = new HashMap();
 		map.put("list", list);
 		return map;
 	}
 	
 	// 1년 단위 특정지점 월별 매출
-	@GetMapping("monthlysales/{storeid}/{year}")
+	@GetMapping("monthlysales/{storeid}/{year}") // * year 나오게 추가 
 	public Map getByStoreMonthSales(@PathVariable("storeid") String storeid, @PathVariable("year") int year) {
-		ArrayList<Object[]> list = service.getByStoreMonthlySales(storeid, year);
+		ArrayList<Map<String, Object[]>> list = service.getByStoreMonthlySales(storeid, year);
 		Map map = new HashMap();
 		map.put("list", list);
 		return map;
 	}
 	
 	// 일간 매출 TOP3 지점
-	@GetMapping("/dailyrank3")
+	@GetMapping("/dailyrank3") // * year 나오게 추가
 	public Map getByDayRank3() {
-		ArrayList<Object[]> list = service.getByDailyRank3();
+		ArrayList<Map<String, Object[]>> list = service.getByDailyRank3();
 		Map map = new HashMap(); 
 		map.put("list", list);
 		return map;
 	}
 	
 	// 월간 매출 TOP3 지점
-	@GetMapping("/monthlyrank3")
+	@GetMapping("/monthlyrank3") // 
 	public Map getByMonthRank3() {
-		ArrayList<Object[]> list = service.getByMonthlyRank3();
+		ArrayList<Map<String, Object[]>> list = service.getByMonthlyRank3();
 		Map map = new HashMap();
 		map.put("list", list);
 		return map;
 	}
 
 	// 일간 전체 매출
-	@GetMapping("/dailyrank")
+	@GetMapping("/dailyrank") // * year 나오게 추가
 	public Map getByDayRank() {
-		ArrayList<Object[]> list = service.getByDailyRank();
+		ArrayList<Map<String, Object[]>> list = service.getByDailyRank();
 		Map map = new HashMap(); 
 		map.put("list", list);
 		return map;
 	}
 	
 	// 월간 전체 매출
-	@GetMapping("/monthlyrank")
+	@GetMapping("/monthlyrank") //
 	public Map getByMonthRank() {
-		ArrayList<Object[]> list = service.getByMonthlyRank();
+		ArrayList<Map<String, Object[]>> list = service.getByMonthlyRank();
 		Map map = new HashMap();
 		map.put("list", list);
 		return map;
 	}
 	
 	// 설정 기간 동안의 전체지점 연매출 조회
-	@GetMapping("/yearlysales/{year1}/{year2}")
+	@GetMapping("/yearlysales/{year1}/{year2}") //
 	public Map getByYearlySales(@PathVariable("year1") int year1, @PathVariable("year2") int year2) {
-		ArrayList<Object[]> list = service.getByYearlySales(year1, year2);
+		ArrayList<Map<String, Object[]>> list = service.getByYearlySales(year1, year2);
 		Map map = new HashMap();
 		map.put("list", list);
 		return map;
 	} 
 	
 	// 설정 기간 동안의 특정지점 연매출 조회
-	@GetMapping("/yearlysales/{storeid}/{year1}/{year2}")
+	@GetMapping("/yearlysales/{storeid}/{year1}/{year2}") //
 	public Map getByStoreYearlySales(@PathVariable("storeid") String storeid, @PathVariable("year1") int year1, @PathVariable("year2") int year2) {
-		ArrayList<Object[]> list = service.getByStoreYearlySales(storeid, year1, year2);
+		ArrayList<Map<String, Object[]>> list = service.getByStoreYearlySales(storeid, year1, year2);
 		Map map = new HashMap();
 		map.put("list", list);
 		return map;
 	}
+
 	
-	
-	@GetMapping("/monthlysales/{storeid}/{year}/{month}")
+	@GetMapping("/monthlysales/{storeid}/{year}/{month}") //ㄴ
 	public Map getByYearAndMonthSales(@PathVariable("storeid") String storeid, @PathVariable("year") int year, @PathVariable("month") int month) {
-		ArrayList<Object[]> list = service.getByStoreYearlySales(storeid, year, month);
+		ArrayList<Map<String, Object[]>> list = service.getByYearAndMonthSales(storeid, year, month);
 		Map map = new HashMap();
 		map.put("list", list);
 		return map;
