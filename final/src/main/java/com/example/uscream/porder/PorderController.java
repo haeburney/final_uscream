e42fffda08ab87679d37fcdacf9e0619d9553d23
YSH <shyang1208@gmail.com>
2023-06-28 09:19:03 +0900

    Porder update
    20230628 09:18
    
    Porder update

--- final/src/main/java/com/example/uscream/porder/PorderController.java
+++ final/src/main/java/com/example/uscream/porder/PorderController.java
@@ -2,7 +2,6 @@ package com.example.uscream.porder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.uscream.store.Store;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -43,12 +44,14 @@ public class PorderController {
 		
 	}
 	
-	@GetMapping("/detail/{ordernum}")
-	public Map getByOrdernum(@PathVariable("ordernum") String ordernum) {
+	@GetMapping("/detail/{storeid}/{orderdate}")
+	public Map getByOrdernum(@PathVariable("storeid") String storeid,@PathVariable("orderdate") String orderdate) {
+		String ordernum = orderdate+"#"+storeid;
+		System.out.println(ordernum);
 		ArrayList<PorderDto> dlist = service.getByOrderNum(ordernum);
 		Map map = new HashMap();
 		System.out.println(dlist);
-		map.put("Storeoder",dlist);
+		map.put("Storeorder",dlist);
 		System.out.println(map);
 		return map;
 		
@@ -69,6 +72,18 @@ public class PorderController {
 		return map;
 	}
 	
+	
+//	@GetMapping("/detail/{ordernum}/{storeid}")
+//	public Map getDetail(@PathVariable("ordernum") String ordernum,@PathVariable("storeid") String storeid) {
+//		ArrayList<PorderDto> dlist = service.getStoreOrder(ordernum, storeid);
+//		Map map = new HashMap();
+//		map.put("detail", dlist);
+//		return map;
+//				
+//	
+//	}
+	
+	
 	@PatchMapping("/confirm/{num}")
 	public void confirm(@PathVariable("num") int tempnum) {
 		service.confirm(tempnum);
@@ -77,6 +92,17 @@ public class PorderController {
 	@GetMapping("/orderlist")
 	public Map getOrderLsit() {
 		ArrayList<Map<String, String>> orderlist = service.getOrdernum();
+		System.out.println("orderlist:" +orderlist);
+		Map<String, Object> map = new HashMap();
+		map.put("orderlist", orderlist);
+		System.out.println("map"+map);
+		return map;
+		
+	}
+	@GetMapping("/orderlist/{store}")
+	public Map getStoreOrderLsit(@PathVariable("store") String store) {
+		System.out.println(store);
+		ArrayList<Map<String, String>> orderlist = service.getStoreOrderlist(store);
 		System.out.println("orderlist:" +orderlist);
 		Map<String, Object> map = new HashMap();
 		map.put("orderlist", orderlist);
