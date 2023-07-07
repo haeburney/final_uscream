875e15a25621bc3a4a62f81d15a7b9613663f82c
BangHyeonJoong <eterion1@naver.com>
2023-07-07 17:46:21 +0900

    Update Notice.java

--- final/src/main/java/com/example/uscream/notice/Notice.java
+++ final/src/main/java/com/example/uscream/notice/Notice.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
